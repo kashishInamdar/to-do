56d940fce89093031d64297df522bb25eadbe9e8
kashishInamdar <kashishinamdar2004@gmail.com>
2023-10-14 17:53:46 +0530

    Add Task Form Lisk

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "./Card.css"
 
-export default function Card({title , description , priority ,id} ){
+export default function Card({title  , description , priority ,id} ){
     return(
         <div className="task-card">
             <h1 className="task">{title}</h1>
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -12,19 +12,28 @@ export default function Home() {
             description: "Nahi to piche rehejaygi",
             priority: "High"
         },
-        {
-            id: 2,
-            title: "learn CRUD Opration",
-            description: "Quch nahi aata yesa bolege",
-            priority: "High"
-        },
-        {
-            id: 1,
-            title: "Complete all opration ",
-            description: "Properly",
-            priority: "to High"
-        },
+       
     ])
+
+    const [title , setTitle] = useState("");
+    const [priority , setPriority] = useState("");
+    const [description , setDescription] = useState(""); 
+
+    const AddToList = ()=>{
+        const random = Math.floor(Math.random() * 100)
+        const obj = {
+            id : random,
+            title : title ,
+            description : description,
+            priority : priority
+        }
+        setTaskList([...taskList , obj]);
+
+        setDescription('');
+        setTitle('');
+        setPriority("")
+
+    }
     
 
     return (
@@ -38,8 +47,8 @@ export default function Home() {
                             const { title, description, priority, id } = tasks;
 
                             return (
-                                <div>
-                                    <Card id={id} title={title} description={description} priority={priority} />
+                                <div key={index}>
+                                    <Card id={id} title={title} description={description} priority={priority}   />
                                 </div>
                             )
                         })
@@ -49,22 +58,42 @@ export default function Home() {
             </div>
 
             <div className="add-task">
-                <h1 className="heading">Add Task</h1>
+                <h1 className="heading">Add Task </h1>
                 <form className="task-input">
+                
+                    <input type="text"
+                        placeholder="Task"
+                        className="inputs"
+                        value={title}
+                        onChange={(e)=>{
+                            setTitle(e.target.value)
+                        }}
+                      
+                    />
+                  
                     <input type="text"
                         className="inputs"
-
+                        placeholder="description"
+                        value={description}
+                        onChange={(e)=>{
+                            setDescription(e.target.value)
+                        }}
                     />
-
+                    
                     <input type="text"
                         className="inputs"
-
+                        placeholder="priority"
+                        value={priority}
+                        onChange={(e)=>{
+                            setPriority(e.target.value)
+                        }}
                     />
 
-                    <input type="text"
-                        className="inputs"
-
-                    />
+                    <button
+                    type="button"
+                     className="button"
+                     onClick={AddToList}
+                     >ADD</button>
 
                 </form>
 
